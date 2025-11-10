3c967ab6aef9f8278e7343b650e529b594bad1ae
lkogomes <luan.gomes@sead.se.gov.br>
2025-11-10 15:36:08 -0300

    Up

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000 // Define a porta para 3000
-    // A propriedade "host" foi removida para voltar ao padrão (localhost)
-  }
+    port: 3000
+  },
+  base: '/MiniSpotify/'
 })
